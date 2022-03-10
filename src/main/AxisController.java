f7568f95d1382ecbd6477e287315b846e1b0dae9
iiotakamasa <tiio@mail.doshisha.ac.jp>
2022-03-10 09:09:32 +0900

    Updated parameters for Dog.

--- src/main/AxisController.java
+++ src/main/AxisController.java
@@ -19,7 +19,27 @@ public class AxisController {
 
 	public Map<Byte, Short> read() {
 		Map<Byte, Short> map = new HashMap<Byte, Short>();
-		Byte[] ids = {(byte)1, (byte)3, (byte)5, (byte)7, (byte)8};
+		Byte[] ids = new Byte[0];
+
+		if      (Params.robotType.equals("CommU")) {
+			Byte[] temp = {(byte)1, (byte)2, (byte)3,  (byte)4,  (byte)5,  (byte)6,  (byte)7,
+				           (byte)8, (byte)9, (byte)10, (byte)11, (byte)12, (byte)13, (byte)14};
+			ids = temp.clone();
+		}
+		else if (Params.robotType.equals("Sota")) {
+			Byte[] temp = {(byte)1, (byte)2, (byte)3, (byte)4,
+						   (byte)5, (byte)6, (byte)7, (byte)8};
+			ids = temp.clone();
+		}
+		else if (Params.robotType.equals("Dog")) {
+			Byte[] temp = {(byte)1, (byte)3, (byte)5, (byte)7, (byte)8};
+			ids = temp.clone();
+		}
+		else {
+			System.err.println("Robot type is wrong. Current robot type is '" + Params.robotType + "'");
+			System.exit(0);
+		}
+
 		Short[] vals = motion.getReadpos();
 		if (vals == null){
 			return map;
